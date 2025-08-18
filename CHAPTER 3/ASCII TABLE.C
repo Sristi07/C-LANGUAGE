22e162b12ec60ccfb05ee0cab52271b724160006
SRISTI SINHA <sristisinha07work@gmail.com>
2025-08-18 19:47:06 +0530

    Add files via upload

---
+++ CHAPTER 3/ASCII TABLE.C
@@ -0,0 +1,103 @@
+ Basic ASCII table that includes characters from the standard ASCII set, which ranges from 0 to 127:
+
+ Dec | Hex | Oct | Char | Description
+-----|-----|-----|------|-------------
+  0  |  00 |  00 |  NUL | Null character
+  1  |  01 |  01 |  SOH | Start of Header
+  2  |  02 |  02 |  STX | Start of Text
+  3  |  03 |  03 |  ETX | End of Text
+  4  |  04 |  04 |  EOT | End of Transmission
+  5  |  05 |  05 |  ENQ | Enquiry
+  6  |  06 |  06 |  ACK | Acknowledge
+  7  |  07 |  07 |  BEL | Bell
+  8  |  08 |  08 |  BS  | Backspace
+  9  |  09 |  09 |  HT  | Horizontal Tab
+ 10  |  0A |  0A |  LF  | Line Feed
+ 11  |  0B |  0B |  VT  | Vertical Tab
+ 12  |  0C |  0C |  FF  | Form Feed
+ 13  |  0D |  0D |  CR  | Carriage Return
+ 14  |  0E |  0E |  SO  | Shift Out
+ 15  |  0F |  0F |  SI  | Shift In
+ 16  |  10 |  10 |  DLE | Data Link Escape
+ 17  |  11 |  11 |  DC1 | Device Control 1
+ 18  |  12 |  12 |  DC2 | Device Control 2
+ 19  |  13 |  13 |  DC3 | Device Control 3
+ 20  |  14 |  14 |  DC4 | Device Control 4
+ 21  |  15 |  15 |  NAK | Negative Acknowledge
+ 22  |  16 |  16 |  SYN | Synchronous Idle
+ 23  |  17 |  17 |  ETB | End of Transmission Block
+ 24  |  18 |  18 |  CAN | Cancel
+ 25  |  19 |  19 |  EM  | End of Medium
+ 26  |  1A |  1A |  SUB | Substitute
+ 27  |  1B |  1B |  ESC | Escape
+ 28  |  1C |  1C |  FS  | File Separator
+ 29  |  1D |  1D |  GS  | Group Separator
+ 30  |  1E |  1E |  RS  | Record Separator
+ 31  |  1F |  1F |  US  | Unit Separator
+ 32  |  20 |  40 |      | Space
+ 33  |  21 |  41 |  !   | Exclamation Mark
+ 34  |  22 |  42 |  "   | Quotation Mark
+ 35  |  23 |  43 |  #   | Number Sign
+ 36  |  24 |  44 |  $   | Dollar Sign
+ 37  |  25 |  45 |  %   | Percent Sign
+ 38  |  26 |  46 |  &   | Ampersand
+ 39  |  27 |  47 |  '   | Apostrophe
+ 40  |  28 |  50 |  (   | Left Parenthesis
+ 41  |  29 |  51 |  )   | Right Parenthesis
+ 42  |  2A |  52 |  *   | Asterisk
+ 43  |  2B |  53 |  +   | Plus Sign
+ 44  |  2C |  54 |  ,   | Comma
+ 45  |  2D |  55 |  -   | Hyphen
+ 46  |  2E |  56 |  .   | Period
+ 47  |  2F |  57 |  /   | Slash
+ 48  |  30 |  60 |  0   | Digit 0
+ 49  |  31 |  61 |  1   | Digit 1
+ 50  |  32 |  62 |  2   | Digit 2
+ 51  |  33 |  63 |  3   | Digit 3
+ 52  |  34 |  64 |  4   | Digit 4
+ 53  |  35 |  65 |  5   | Digit 5
+ 54  |  36 |  66 |  6   | Digit 6
+ 55  |  37 |  67 |  7   | Digit 7
+ 56  |  38 |  70 |  8   | Digit 8
+ 57  |  39 |  71 |  9   | Digit 9
+ 58  |  3A |  72 |  :   | Colon
+ 59  |  3B |  73 |  ;   | Semicolon
+ 60  |  3C |  74 |  <   | Less Than
+ 61  |  3D |  75 |  =   | Equals
+ 62  |  3E |  76 |  >   | Greater Than
+ 63  |  3F |  77 |  ?   | Question Mark
+ 64  |  40 |  100|  @   | At Sign
+ 65  |  41 |  101|  A   | Uppercase A
+ 66  |  42 |  102|  B   | Uppercase B
+ 67  |  43 |  103|  C   | Uppercase C
+ 68  |  44 |  104|  D   | Uppercase D
+ 69  |  45 |  105|  E   | Uppercase E
+ 70  |  46 |  106|  F   | Uppercase F
+ 71  |  47 |  107|  G   | Uppercase G
+ 72  |  48 |  110|  H   | Uppercase H
+ 73  |  49 |  111|  I   | Uppercase I
+ 74  |  4A |  112|  J   | Uppercase J
+ 75  |  4B |  113|  K   | Uppercase K
+ 76  |  4C |  114|  L   | Uppercase L
+ 77  |  4D |  115|  M   | Uppercase M
+ 78  |  4E |  116|  N   | Uppercase N
+ 79  |  4F |  117|  O   | Uppercase O
+ 80  |  50 |  120|  P   | Uppercase P
+ 81  |  51 |  121|  Q   | Uppercase Q
+ 82  |  52 |  122|  R   | Uppercase R
+ 83  |  53 |  123|  S   | Uppercase S
+ 84  |  54 |  124|  T   | Uppercase T
+ 85  |  55 |  125|  U   | Uppercase U
+ 86  |  56 |  126|  V   | Uppercase V
+ 87  |  57 |  127|  W   | Uppercase W
+ 88  |  58 |  130|  X   | Uppercase X
+ 89  |  59 |  131|  Y   | Uppercase Y
+ 90  |  5A |  132|  Z   | Uppercase Z
+ 91  |  5B |  133|  [   | Left Square Bracket
+ 92  |  5C |  134|  \   | Backslash
+ 93  |  5D |  135|  ]   | Right Square Bracket
+ 94  |  5E |  136|  ^   | Caret
+ 95  |  5F |  137|  _   | Underscore
+ 96  |  60 |  140|  `   | Grave Accent
+ 97  |  61 |  141|  a   | Lowercase a
+ 98  |  62 |  142|  b   | Lowercase b
