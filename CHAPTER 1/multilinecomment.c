bc71c7df1a754fad7e152805b0119598c8680bd5
SRISTI SINHA <sristisinha07work@gmail.com>
2025-08-18 19:30:10 +0530

    Add files via upload

---
+++ CHAPTER 1/multilinecomment.c
@@ -0,0 +1,9 @@
+/*
+program: multilinecomment
+*/
+#include<stdio.h>
+
+int main(){
+    printf("Hello World");
+    return 0;
+}
---
+++ CHAPTER 1/singlelinecomment.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    //I am writing this just for fun.This line doesn't do anything
+    printf("Hello World");
+    return 0;
+}
